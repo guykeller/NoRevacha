1d74852d9ac6d80c5f9be1d03fbc235575057fbe
Guy Keller <Guy Keller@DESKTOP-R2ADU5B>
2018-09-08 19:49:06 +0300

    changes

--- noRevacha.js
+++ noRevacha.js
@@ -3,35 +3,46 @@ $(document).ready(function () {
     let d = new Date();
     d.setMonth(6);
     d.setDate(1);
-    //let curShouldAsk = true;
     let savedNextDateToAsk = chrome.storage.sync.get('nextDateToAsk', function () {});
     let nextDayToAsk =  (savedNextDateToAsk) ? savedNextDateToAsk : d;
     chrome.storage.sync.set({ 'nextDateToAsk' : nextDayToAsk });
-    //let prevShouldAsk = chrome.storage.sync.get('shouldAsk', function () {});
 
-    //TODO: change to actual dates
-    if(Date.now() >= nextDayToAsk){//checking if month is between July and August
-        dialog = $("<p>האם תרצה לבטל את דמי הרווחה?</p>").dialog({
+    if(Date.now() >= nextDayToAsk){
+        dialog = $("<p>No revacha ?</p>").dialog({
+            resizable: false,
+            height:140,
+            modal: true,
             buttons: {
-                "Yes": cancelRevachaNow(),
-                "No": stopAsking(),
-                "Snooze":snooze()
+                "Yes": function() { dialog.dialog('close'); cancelRevachaNow();},
+                "No": function() { dialog.dialog('close');  stopAsking()},
+                "Snooze": function()  { dialog.dialog('close');  snooze()}
             }
         });
     }
 
     function cancelRevachaNow(){
         let clickDestination = document.getElementById("li19").getAttribute("onclick");
-        if (clickDestination.contains("Revacha")) {
+        if (clickDestination.includes("Revacha")) {
+
+
+            document.getElementById('jordan').contentWindow.onload = function(){
+                let cancelCheckboxButton = document.getElementById('jordan').contentWindow.document.getElementById('ckIshur');
+
+
+                cancelCheckboxButton.click();
+                let cancelApproveButton = document.getElementById('jordan').contentWindow.document.getElementById('btnishur');
+                cancelApproveButton.click();
+                let areYouSureButton = document.getElementById('jordan').contentWindow.document.getElementsByClassName('ui-button');
+                areYouSureButton[2].click(); //clicking on the right button from the buttons array
+                stopAsking();
+                window.history.back();
+            }();
+
             document.getElementById("li19").click();
-            let cancelCheckboxButton = document.getElementById('jordan').contentWindow.document.getElementById('ckIshur');
-            cancelCheckboxButton.click();
-            let cancelApproveButton = document.getElementById('jordan').contentWindow.document.getElementById('btnishur');
-            cancelApproveButton.click();
-            let areYouSureButton = document.getElementById('jordan').contentWindow.document.getElementsByClassName('ui-button');
-            areYouSureButton[2].click(); //clicking on the right button from the buttons array
-            stopAsking();
-            window.history.back();
+
+
+
+
         }
         else {
             alert("Our apologies, could not cancel Revacha. Go to personal info to cancel manually");
@@ -44,7 +55,6 @@ $(document).ready(function () {
         let newD = new Date();
         let curD = Date.now();
         newD.setDate(curD.getDate()+7);
-        dialog.close();
     }
 
     function stopAsking() {
@@ -52,7 +62,5 @@ $(document).ready(function () {
         let curD = Date.now();
         newD.setFullYear(curD.getFullYear()+1, 6, 1);
         chrome.storage.sync.set({ 'nextDateToAsk' : newD });
-        dialog.close();
     }
 })
-
